5cfcc68799e2c7c74f5dd3686656f8ea99d5f906
Miles Standoffish <toddnan@gmail.com>
2019-06-23 23:52:22 -0500

    click event to choose character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,9 +37,16 @@ if ($(document).ready()) {
         let $newCharacter = $("<div>");
         $newCharacter.text(element.name);
         $newCharacter.attr("class", "sm-character");
+        $newCharacter.attr("name", element.name.toLowerCase().replace(" ","-"));
         let $characterImage = $("<img>");
         $characterImage.attr("src", "assets/images/" + element.image);
         $newCharacter.prepend($characterImage);
         $("#all-characters").append($newCharacter);
     });
 }
+
+$(".sm-character").on("click", function () {
+    console.log("clicked " + $(this).attr("name"));
+    var element = $(this).detach();
+    $("#chosen-character").append(element);
+});
